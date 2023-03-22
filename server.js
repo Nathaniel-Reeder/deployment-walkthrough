b1165889be61d52147de98ce9cd0e7450be74c44
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-03-22 12:26:24 -0600

    now serves front end files from back end endpoints

--- server.js
+++ server.js
@@ -5,5 +5,16 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.listen(5150, () => console.log('app is up on 5150'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/styles.css')
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(__dirname + "/public/main.js")
+})
+
+app.listen(5150, () => console.log('app is up on 5150'))
